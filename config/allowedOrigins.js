919b5d263f5f761fd87ebe037b7ecbb8c84e3b0c
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2023-09-03 16:47:37 +0530

    all origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3001',
     'http://localhost:3000',
-    'https://saquibshakeel.github.io/pets-connect/',
-    'https://pets-connect.onrender.com/',
+    'https://saquibshakeel.github.io/pets-connect',
+    'https://pets-connect.onrender.com',
     '*'
 ];
 
