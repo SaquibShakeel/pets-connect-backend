b41749b51d1bf0ab2e8d14a9927476eaa4f63b00
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2023-09-04 02:15:12 +0530

    vercel origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://saquibshakeel.github.io/pets-connect',
     'https://pets-connect.onrender.com',
+    'https://pets-connect-pink.vercel.app',
     '*'
 ];
 
