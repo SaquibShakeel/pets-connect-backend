acd38b0c200518c76eddfe6094df831587390552
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2023-09-03 16:09:52 +0530

    render origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3001',
+    'http://localhost:3000',
     'https://saquibshakeel.github.io/pets-connect/',
+    'https://pets-connect.onrender.com/',
 ];
 
 module.exports = allowedOrigins;
